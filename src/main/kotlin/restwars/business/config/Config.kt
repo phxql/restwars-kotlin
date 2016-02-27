e149fd4fa3a66a7f474758bff624395ca0f06d95
Moritz Kammerer <kammerer.moritz@gmail.com>
2016-02-27 12:59:42 +0100

    Now using Jackson for YAML parsing.

--- src/main/kotlin/restwars/business/config/Config.kt
+++ src/main/kotlin/restwars/business/config/Config.kt
@@ -1,7 +1,8 @@
 package restwars.business.config
 
-import org.yaml.snakeyaml.Yaml
-import org.yaml.snakeyaml.constructor.Constructor
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import restwars.business.planet.Resources
 import java.nio.file.Files
 import java.nio.file.Path
@@ -13,35 +14,35 @@ data class StarterPlanet(val resources: Resources)
 data class NewPlanet(val resources: Resources)
 
 data class Config(val universeSize: UniverseSize, val starterPlanet: StarterPlanet, val newPlanet: NewPlanet, val roundTime: Int) {
-    data class UniverseSizeDto(var maxGalaxies: Int = 0, var maxSystems: Int = 0, var maxPlanets: Int = 0) {
+    data class UniverseSizeDto(val maxGalaxies: Int, val maxSystems: Int, val maxPlanets: Int) {
         fun toUniverseSize(): UniverseSize {
             return UniverseSize(maxGalaxies, maxSystems, maxPlanets)
         }
     }
 
-    data class ResourcesDto(var crystal: Int = 0, var gas: Int = 0, var energy: Int = 0) {
+    data class ResourcesDto(val crystal: Int, val gas: Int, val energy: Int) {
         fun toResources(): Resources {
             return Resources(crystal, gas, energy)
         }
     }
 
-    data class StarterPlanetDto(var resources: ResourcesDto = ResourcesDto()) {
+    data class StarterPlanetDto(val resources: ResourcesDto) {
         fun toStarterPlanet(): StarterPlanet {
             return StarterPlanet(resources.toResources())
         }
     }
 
-    data class NewPlanetDto(var resources: ResourcesDto = ResourcesDto()) {
+    data class NewPlanetDto(val resources: ResourcesDto) {
         fun toNewPlanet(): NewPlanet {
             return NewPlanet(resources.toResources())
         }
     }
 
     data class ConfigDto(
-            var universeSize: UniverseSizeDto = UniverseSizeDto(),
-            var starterPlanet: StarterPlanetDto = StarterPlanetDto(),
-            var newPlanet: NewPlanetDto = NewPlanetDto(),
-            var roundTime: Int = 0) {
+            val universeSize: UniverseSizeDto,
+            val starterPlanet: StarterPlanetDto,
+            val newPlanet: NewPlanetDto,
+            val roundTime: Int) {
         fun toConfig(): Config {
             return Config(universeSize.toUniverseSize(), starterPlanet.toStarterPlanet(), newPlanet.toNewPlanet(), roundTime)
         }
@@ -49,11 +50,12 @@ data class Config(val universeSize: UniverseSize, val starterPlanet: StarterPlan
 
     companion object {
         fun loadFromFile(path: Path): Config {
-            val yaml = Yaml(Constructor(ConfigDto::class.java))
-            val configDto = Files.newBufferedReader(path).use {
-                yaml.load(it) as ConfigDto
-            }
-            return configDto.toConfig()
+            val mapper = ObjectMapper(YAMLFactory()) // Enable YAML parsing
+            mapper.registerModule(KotlinModule()) // Enable Kotlin support
+
+            return Files.newBufferedReader(path).use {
+                mapper.readValue(it, ConfigDto::class.java)
+            }.toConfig()
         }
     }
 }
