369152db82a6047ce5f16d5e6c5a39b148ba0e93
Moritz Kammerer <kammerer.moritz@gmail.com>
2016-03-06 20:51:20 +0100

    Fix typo.

--- api/src/main/kotlin/restwars/rest/api/Config.kt
+++ api/src/main/kotlin/restwars/rest/api/Config.kt
@@ -1,6 +1,6 @@
 package restwars.rest.api
 
-data class UniverseSizeResponse(val minGalaxy: Int, val maxGalaxy: Int, val minSystem: Int, val maxSystem: Int, val minPLanet: Int, val maxPlanet: Int)
+data class UniverseSizeResponse(val minGalaxy: Int, val maxGalaxy: Int, val minSystem: Int, val maxSystem: Int, val minPlanet: Int, val maxPlanet: Int)
 
 data class ConfigResponse(val roundTime: Int, val universeSize: UniverseSizeResponse) : Result {
     companion object {}
